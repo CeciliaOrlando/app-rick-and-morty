7a321015b9f092971f2309c37b70ad0c85897873
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-03-07 10:09:25 -0300

    o

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+// postcss.config.mjs
+export default {
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+  ],
 };
-
-export default config;
