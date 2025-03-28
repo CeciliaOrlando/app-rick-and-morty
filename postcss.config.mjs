ac541293d333f26e2eeeeefbfebb50798570c5e7
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-03-28 10:24:27 -0300

    deploye

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,10 @@
 // postcss.config.mjs
+import tailwindcss from '@tailwindcss/postcss7-compat';
+import autoprefixer from 'autoprefixer';
+
 export default {
   plugins: [
-    'tailwindcss',
-    'autoprefixer',
+    tailwindcss,
+    autoprefixer,
   ],
 };
